0f09c32fc3148d44d5f6b38fb9a5c637f7409fcc
ginov777 <78718146+ginov777@users.noreply.github.com>
2021-02-24 16:35:18 -0300

    Update juego.cpp
    Modularicé la ubicación de los personajes. Falta MOSTRAR EL TABLERO POR PANTALLA, donde puse el comentario en la función posicionarPersonaje()

--- TP 3/juego.cpp
+++ TP 3/juego.cpp
@@ -1,84 +1,96 @@
 #include "juego.h"
 
 Juego::Juego() {
-  cantidadPersonajesUno = 0;
-  cantidadPersonajesDos = 0;
-  // mapaPartida. (crear tablero)
+    cantidadPersonajesUno = 0;
+    cantidadPersonajesDos = 0;
+    mapaPartida.mapear("mapa.csv"); //crea el tablero
 }
 
 void Juego::definirPreliminares(Mapa* mapa) {
-  menuPartida.procesarArchivo();
-  menuPartida.procesarMenuPrincipal(
-      estadoJuego);  // esto está bien o debo usar el método asignarEstado?
+    menuPartida.procesarArchivo();
+    menuPartida.procesarMenuPrincipal(
+            estadoJuego);  // esto está bien o debo usar el método asignarEstado?
 }
 
 
-void Juego::coorValidas(Coordenada* coor,){
-	int fila,columna;
-	bool valida = false;
-	while(!valida){
-		cout<<"Ingrese fila: ";
-		cin>>fila;
-		while(fila < 1 || fila > 8){
-			cout<<"valor incorrecto, debe ingresar una fila entre 1 y 8: ";
-			cin>>*fila;
-		}
-		cout<<"Ingrese columna: ";
-		cin>>columna;		
-		while(columna < 1 || columna > 8){
-			cout<<"valor incorrecto, debe ingresar una columna entre 1 y 8: ";
-			cin>>*columna;
-		}
-		coor->cambiarFilaYColumna(fila,columna);
-		if(mapaPartida->ocupado(coor))
-			cout<<"El lugar elegido se encuentra ocupado, vuelva a elegir uno";
-		else
-			valida = true;	
-	}
+bool Juego::filaValida(int fila){
+    return (fila >= MINIMO_TABLERO && fila <= MAXIMO_TABLERO);
+}
+
+bool Juego::columnaValida(int columna){
+    return (columna >= MINIMO_TABLERO && columna <= MAXIMO_TABLERO);
+}
+
+void Juego::pedirFila(int* fila){
+    cout << "Ingrese fila: ";
+    cin >> fila;
+    while (!filaValida(*fila)){
+        cout << "Fila ingresada en el rango incorrecto, debe ingresar una fila entre 1 y 8: ";
+        cin >> fila;
+    }
+}
+
+void Juego::pedirColumna(int* columna){
+    cout << "Ingrese columna: ";
+    cin >> columna;
+    while (!columnaValida(*columna)){
+        cout << "Columna ingresada en el rango incorrecto, debe ingresar una columna entre 1 y 8: ";
+        cin >> columna;
+    }
+}
+
+void Juego::consultarCoordenada(Coordenada* coor, Personaje* personajes[MAX_PERSONAJES], int* i){
+    int fila, columna;
+    bool puedoConsultar =  true;
+    while (puedoConsultar){
+        cout << "Indique las coordenadas donde desea posicionar a "<< personajes[*i]->obtenerNombre() << ":" << endl;
+        pedirFila(&fila);
+        pedirColumna(&columna);
+        coor->cambiarFilaYColumna(fila,columna);
+        if(mapaPartida->ocupado(coor)) {
+            cout << "El lugar elegido se encuentra ocupado, vuelva a elegir uno.";
+        } else {
+            puedoConsultar = false;
+        }
+    }
 }
 
 
+
+void Juego::posicionarPersonaje(Personaje* personajes[MAX_PERSONAJES], int* i){
+    Coordenada* coor = new Coordenada(0,0);
+    //MUESTRO EL MAPA POR PANTALLA
+    consultarCoordenada(coor,personajes,i);
+    personajes[*i]->asignarCoordenada(coor->obtenerFila(),coor->obtenerColumna());
+    mapaPartida->consulta(*coor)->obtenerDato()->ocupar(personajes[*i]);
+    (*i)++;
+    delete coor;
+}
+
+
+void Juego::procesarUbicacion(Personaje* personajesPrimero[MAX_PERSONAJES], int topeUno, Personaje* personajesSegundo[MAX_PERSONAJES], int topeDos) {
+    int i = 0, j = 0;
+    while ( (i < topeUno) && (j < topeDos)) { //acá tengo en cuenta que ambos jugadores tienen exactamente 3 personajes elegidos
+        posicionarPersonaje(personajesPrimero,&i);
+        posicionarPersonaje(personajesSegundo,&j);
+    }
+}
 
 void Juego::ubicarPersonajes() {
-  // IMprimir tablero
-  int i;
-  Coordenada* coor = new Coordenada(0,0);
-	
-	
-	
-	
-  
-  bool* empiezaUno;
-  if(empiezaUno){
-      for (int i = 0; i < MAX_PERSONAJES; ++i) {          
-	  cout << "Jugador 1, indique las coordenadas donde desea posicionar a "<<personajesJugadorUno[i]->obtenerNombre() <<":" << endl; 
-	  coorValidas(coor,i);
-	  personajesJugadorUno[i]->asignarCoordenada(coor->obtenerFila(),coor->obtenerColumna());
-	  mapaPartida->consulta(*coor)->obtenerDato()->ocupar(personajesJugadorUno[i]);
-
-      cout << "Jugador 2, indique en que fila desea posicionar a " <<personajesJugadorDos[i]->obtenerNombre() <<":" << endl;
-	  coorValidas(coor);
-	  personajesJugadorDos[i]->asignarCoordenada(fila,columna);
-	  mapaPartida->consulta(*coor)->obtenerDato()->ocupar(personajesJugadorUno[i]);
-      }
-  }
-  else{
- 	  cout << "Jugador 2, indique en que fila desea posicionar a " <<personajesJugadorDos[i]->obtenerNombre() <<":" << endl;
-	  coorValidas(&fila,&columna);
-	  personajesJugadorDos[i]->asignarCoordenada(fila,columna);
-	  mapaPartida->consulta(*coor)->obtenerDato()->ocupar(personajesJugadorUno[i]);
-	  
-	  cout << "Jugador 1, indique las coordenadas donde desea posicionar a "<<personajesJugadorUno[i]->obtenerNombre() <<":" << endl; 
-	  coorValidas(&fila,&columna);
-	  personajesJugadorUno[i]->asignarCoordenada(fila,columna);
-	  mapaPartida->consulta(*coor)->obtenerDato()->ocupar(personajesJugadorUno[i]);
-  }
-   
+    bool empiezaUno;
+    elegirPrimerLugar(&empiezaUno);
+    if (empiezaUno) {
+        imprimirMensajeUno();
+        procesarUbicacion(personajesJugadorUno, cantidadPersonajesUno, personajesJugadorDos, cantidadPersonajesDos);
+    } else {
+        imprimirMensajeDos();
+        procesarUbicacion(personajesJugadorDos, cantidadPersonajesDos, personajesJugadorUno, cantidadPersonajesUno);
+    }
 }
 
 void Juego::elegirPersonajes() {
-  menuPartida.procesarMenuJuego(personajesJugadorUno, &cantidadPersonajesUno, personajesJugadorDos, &cantidadPersonajesDos);
-  ubicarPersonajes();
+    menuPartida.procesarMenuJuego(personajesJugadorUno, &cantidadPersonajesUno, personajesJugadorDos, &cantidadPersonajesDos);
+    ubicarPersonajes();
 }
 
 void Juego::elegirPrimerLugar(bool* empiezaUno) { (*empiezaUno) = rand() % 2; }
@@ -86,126 +98,136 @@ void Juego::elegirPrimerLugar(bool* empiezaUno) { (*empiezaUno) = rand() % 2; }
 Mapa Juego::obtenerMapa() { return mapaPartida; }
 
 int Juego::contarMuertos(Personaje* personajes[MAX_PERSONAJES], int tope) {
-  int muertos;
-  for (int i = 0; i < tope; i++) {
-    if (personajes[i]->obtenerVida() == 0) muertos++;
-  }
-  return muertos;
+    int muertos;
+    for (int i = 0; i < tope; i++) {
+        if (personajes[i]->obtenerVida() == 0) muertos++;
+    }
+    return muertos;
 }
 
 bool Juego::consultaEliminado() {
-  if (contarMuertos(personajesJugadorUno, cantidadPersonajesUno) == cantidadPersonajesUno) {
-    return true;
-  } else if (contarMuertos(personajesJugadorDos, cantidadPersonajesDos) == cantidadPersonajesDos) {
-    return true;
-  } else {
-    return false;
-  }
+    if (contarMuertos(personajesJugadorUno, cantidadPersonajesUno) == cantidadPersonajesUno) {
+        return true;
+    } else if (contarMuertos(personajesJugadorDos, cantidadPersonajesDos) == cantidadPersonajesDos) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-void Juego::asignarEstadoJuego(char estado){ 
-	estadoJuego = estado; 
+void Juego::asignarEstadoJuego(char estado){
+    estadoJuego = estado;
 }
 
 // si entra acá es porque hay alguno con vida
 void Juego::jugarTurno(Personaje* personajes[MAX_PERSONAJES], int topeUno, int* i) {
-  if ((*i) >= (topeUno)) {
-    (*i) = 0;
-  }
-  if (personajes[*i]->obtenerVida() != 0) {
-    return menuPartida.procesarTurno(
-        personajes[*i]);  //(Mapa* mapa, Personaje* personaje, Costos* costos)
-  }
-  (*i)++;
-  return jugarTurno(personajes, topeUno, i);
+    if ((*i) >= (topeUno)) {
+        (*i) = 0;
+    }
+    if (personajes[*i]->obtenerVida() != 0) {
+        return menuPartida.procesarTurno(personajes[*i]);  //(Mapa* mapa, Personaje* personaje, Costos* costos)
+    }
+    (*i)++;
+    return jugarTurno(personajes, topeUno, i);
 }
 
 bool Juego::opcionValida(char opcion) {
-  return (opcion == AFIRMATIVO || opcion == NEGATIVO);
+    return (opcion == AFIRMATIVO || opcion == NEGATIVO);
 }
 
 void Juego::pedirGuardado(char* opcion) {
-  cout << "Desea guardar el progreso de la partida y salir del juego? S para Sí, N para no" << endl;
-  cin >> opcion;
-  while (!opcionValida(*opcion)) {
-    cout << "Ingresó un caracter incorrecto. Vuelva a ingresar" << endl;
+    cout << "Desea guardar el progreso de la partida y salir del juego? S para Sí, N para no" << endl;
     cin >> opcion;
-  }
+    while (!opcionValida(*opcion)) {
+        cout << "Ingresó un caracter incorrecto. Vuelva a ingresar" << endl;
+        cin >> opcion;
+    }
 }
 
 void Juego::procesarGuardado(char opcion, bool* guardo) {
-  if (opcion == AFIRMATIVO) {
-    archivoPartida.asignarPath("partida.csv");
-    archivoPartida.escribirArchivo(personajesJugadorUno, cantidadPersonajesUno, personajesJugadorDos, cantidadPersonajesDos);
-  }
+    if (opcion == AFIRMATIVO) {
+        archivoPartida.asignarPath("partida.csv");
+        archivoPartida.escribirArchivo(personajesJugadorUno, cantidadPersonajesUno, personajesJugadorDos, cantidadPersonajesDos);
+    }
 }
 
 void Juego::consultarGuardado(bool* guardo) {
-  char opcion;
-  pedirGuardado(&opcion);
-  procesarGuardado(opcion, guardo);
+    char opcion;
+    pedirGuardado(&opcion);
+    procesarGuardado(opcion, guardo);
 }
 
 void Juego::procesarTurnos(Personaje* personajesPrimero[MAX_PERSONAJES], int topeUno, Personaje* personajesSegundo[MAX_PERSONAJES], int topeDos) {
-  int i = 0, j = 0;
-  bool guardo = false;
-  while (!consultaEliminado() && !guardo) {
-    jugarTurno(personajesPrimero, topeUno, &i);
-    jugarTurno(personajesSegundo, topeDos, &j);
-    if (!consultaEliminado()) {
-      consultarGuardado(&guardo);
+    int i = 0, j = 0;
+    bool guardo = false;
+    while (!consultaEliminado() && !guardo) {
+        jugarTurno(personajesPrimero, topeUno, &i);
+        jugarTurno(personajesSegundo, topeDos, &j);
+        if (!consultaEliminado()) {
+            consultarGuardado(&guardo);
+        }
     }
-  }
-  if (guardo) {
-    asignarEstadoJuego(GUARDADO);
-  } else {
-    asignarEstadoJuego(FINALIZADO);
-  }
+    if (guardo) {
+        asignarEstadoJuego(GUARDADO);
+    } else {
+        asignarEstadoJuego(FINALIZADO);
+    }
 }
 
 void Juego::asignarPersonaje(Personaje* personajes[MAX_PERSONAJES], int* tope, Personaje* personajeNuevo) {
-  personajes[*tope] = personajeNuevo;
-  (*tope)++;
+    personajes[*tope] = personajeNuevo;
+    (*tope)++;
 }
 
 // acá tengo que actualizar la info de los vectoresjugador y de la info de los
 // personajes (no crearlos)
 void Juego::cargarPartida() {
-  string elemento, nombre;
-  int escudo, vida, energia, fila, columna, cant_leidos;
-  while (!archivoPartida.finArchivo()) {
-    archivoPartida.descomponerLineaPartida (&elemento, &nombre, &escudo, &vida, &energia, &fila, &columna);
-    menuPartida.obtenerDiccionario().modificarContenido(nombre, escudo, vida, energia, fila, columna);
-    Personaje* personajeLeido = menuPartida.obtenerDiccionario().consultaNodo(nombre);
-    if (cant_leidos < MAX_PERSONAJES) {
-      asignarPersonaje(personajesJugadorUno, &cantidadPersonajesUno, personajeLeido);
-    } else {
-      asignarPersonaje(personajesJugadorDos, &cantidadPersonajesDos, personajeLeido);
+    string elemento, nombre;
+    int escudo, vida, energia, fila, columna, cant_leidos;
+    while (!archivoPartida.finArchivo()) {
+        archivoPartida.descomponerLineaPartida (&elemento, &nombre, &escudo, &vida, &energia, &fila, &columna);
+        menuPartida.obtenerDiccionario().modificarContenido(nombre, escudo, vida, energia, fila, columna);
+        Personaje* personajeLeido = menuPartida.obtenerDiccionario().consultaNodo(nombre);
+        if (cant_leidos < MAX_PERSONAJES) {
+            asignarPersonaje(personajesJugadorUno, &cantidadPersonajesUno, personajeLeido);
+        } else {
+            asignarPersonaje(personajesJugadorDos, &cantidadPersonajesDos, personajeLeido);
+        }
     }
-  }
 }
 
 void Juego::revisarPartida() {
-  archivoPartida.asignarPath("partida.csv");
-  if (archivoPartida.procesarArchivo()) {
-    cargarPartida();
-  } else {
-    elegirPersonajes();
-  }
+    archivoPartida.asignarPath("partida.csv");
+    if (archivoPartida.procesarArchivo()) {
+        cargarPartida();
+    } else {
+        elegirPersonajes();
+    }
+}
+
+void Juego::imprimirMensajeUno(){
+    cout << "Comenzará el jugador Uno y se irán turnando hasta finalizar." << endl;
+}
+
+void Juego::imprimirMensajeDos(){
+    cout << "Comenzará el jugador Dos y se irán turnando hasta finalizar." << endl;
 }
 
 void Juego::procesarJuego() {
-  definirPreliminares();  
-  if (estadoJuego == JUGANDO) {
-    revisarPartida();
-    bool empiezaUno = rand() % 2;
-    if (empiezaUno) {
-      procesarTurnos(personajesJugadorUno, cantidadPersonajesUno, personajesJugadorDos, cantidadPersonajesDos);
-    } else {
-      procesarTurnos(personajesJugadorDos, cantidadPersonajesDos, personajesJugadorUno, cantidadPersonajesUno);
+    definirPreliminares();
+    if (estadoJuego == JUGANDO) {
+        revisarPartida();
+        bool empiezaUno;
+        elegirPrimerLugar(&empiezaUno);
+        if (empiezaUno) {
+            imprimirMensajeUno();
+            procesarTurnos(personajesJugadorUno, cantidadPersonajesUno, personajesJugadorDos, cantidadPersonajesDos);
+        } else {
+            imprimirMensajeDos();
+            procesarTurnos(personajesJugadorDos, cantidadPersonajesDos, personajesJugadorUno, cantidadPersonajesUno);
+        }
     }
-  }
-  if (estadoJuego == FINALIZADO) {
-    archivoPartida.eliminarArchivo();
-  }
+    if (estadoJuego == FINALIZADO) {
+        archivoPartida.eliminarArchivo();
+    }
 }
