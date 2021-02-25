c4f485bb81486dffc5f26c0bb35752134bb9b2fe
ginov777 <78718146+ginov777@users.noreply.github.com>
2021-02-24 23:54:24 -0300

    Update fuego.cpp

--- TP 3/fuego.cpp
+++ TP 3/fuego.cpp
@@ -1,12 +1,11 @@
 #include "fuego.h"
 
-Fuego::Fuego(string nombre, int escudo, int vida, int fila, int columna)
-    : Personaje(nombre, escudo, vida, fila, columna) {
+Fuego::Fuego(string nombre, int escudo, int vida): Personaje(nombre, escudo, vida) {
   alimento = "madera";
   elemento = "fuego";
 }
 
-string Fuego::obtenerElemento() { return elemento; }
+string Fuego::obtenerElemento() { return "fuego"; }
 
 string Fuego::obtenerAlimento() { return alimento; }
 
@@ -20,7 +19,7 @@ void Fuego::alimentar() {
   }
 
   while (energia < TOPE_ENERGIA &&
-         energiaSuministrada < 10) {  // Ese 10 puede ser constante.
+         energiaSuministrada < ENERGIA_LIMITE) {  // Ese 10 puede ser constante. =10
     energia++;
     energiaSuministrada++;
   }
@@ -36,11 +35,9 @@ int Fuego::obtenerDanio(string elemento) {
 }
 
 void Fuego::ataque(Personaje* enemigos[]) {
-  int i;
   gastarEnergia(ENERGIA_ATAQUE_FUEGO);
-  for (i = 0; i < MAX_PERSONAJES; i++) {
-    if (abs(this->coordenada->obtenerFila() -
-            enemigos[i]->obtenerCoordenada()->obtenerFila()) <= 1) {
+  for (int i = 0; i < MAX_PERSONAJES; i++) {
+    if (abs(this->coordenada->obtenerFila() - enemigos[i]->obtenerCoordenada()->obtenerFila()) <= 1) {
       enemigos[i]->daniar(this->obtenerDanio(enemigos[i]->obtenerElemento()));
     }
   }
