6c3bfa729339ac5eae5889d711ea2c5cc91fa2e8
facundo <facundomilhas@gmail.com>
2021-02-25 00:42:28 -0300

    edu se fue a cagar

---
+++ TP 3/cmake-build-debug/CMakeFiles/tp3.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/agua.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/agua.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/aire.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/aire.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/archivo.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/archivo.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/ataques.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/ataques.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/camino.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/camino.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/casilla.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/casilla.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/coordenada.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/coordenada.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/costos.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/costos.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/diccionario.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/diccionario.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/fuego.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/fuego.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/juego.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/juego.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/lago.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/lago.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/main.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/main.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/mapa.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/mapa.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/menu.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/menu.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/montana.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/montana.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/nodo.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/nodo.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/nodo_grafo.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/nodo_grafo.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/nodo_pila.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/nodo_pila.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/personaje.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/personaje.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/pila.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/pila.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/precipicio.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/precipicio.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/tierra.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/tierra.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/vacio.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/vacio.cpp.o"
+  "/home/facundo/Downloads/TP3-Algo2/TP 3/volcan.cpp" "/home/facundo/Downloads/TP3-Algo2/TP 3/cmake-build-debug/CMakeFiles/tp3.dir/volcan.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
