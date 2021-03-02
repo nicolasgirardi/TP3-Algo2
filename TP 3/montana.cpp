1af8557914c5cbe12e93ec4b2b497a3145a82afb
eduescuti <79531987+eduescuti@users.noreply.github.com>
2021-03-02 12:51:44 -0300

    espaciado corregido

--- TP 3/montana.cpp
+++ TP 3/montana.cpp
@@ -1,14 +1,14 @@
 #include "montana.h"
 
 Montana::Montana() : Casilla() {
-	costo = 1;
+    costo = 1;
 }
 
 string Montana::Tipo() { return "montana"; }
 
 int Montana::obtenerCosto(string elemento) {
-	if (elemento == "tierra") {
-    	return 0;
+    if (elemento == "tierra") {
+        return 0;
   	} else if (elemento == "aire") {
     	return 2;
   	} else {
