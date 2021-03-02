5ffc1ff5336cefa480002d4d0ee3fb847a4e4ffe
eduescuti <79531987+eduescuti@users.noreply.github.com>
2021-03-02 12:48:30 -0300

    espaciado corregido y metodos documentados

--- TP 3/menuJuego.h
+++ TP 3/menuJuego.h
@@ -9,30 +9,35 @@
 
 class MenuJuego: public Menu {
 
-public:
+	public:
 
-    MenuJuego();
+		// Contructor del Menu Juego
+		MenuJuego();
+		
+		// POST: muestra el menu por pantalla
+		void mostrarMenu();
 
+		// POST: procesa la seleccion de personajes
+		void procesarSeleccion(Personaje* jugadorUno[MAX_PERSONAJES],int* topeUno,Personaje* jugadorDos[MAX_PERSONAJES],int* topeDos, Diccionario* diccionarioPersonajes);
 
-    void procesarSeleccion(Personaje* jugadorUno[MAX_PERSONAJES],int* topeUno,Personaje* jugadorDos[MAX_PERSONAJES],int* topeDos, Diccionario* diccionarioPersonajes);
+		// POST: se encarga de seleccionar un personaje del diccionario de personajes,
+		// y se lo asigna al vector de los personajes del jugador.
+		void seleccionarPersonaje(Personaje* jugador[MAX_PERSONAJES], int* tope, string* nombre, Diccionario* diccionarioPersonajes);
 
-    void seleccionarPersonaje(Personaje* jugador[MAX_PERSONAJES], int* tope, string* nombre, Diccionario* diccionarioPersonajes);
+		// POST: verifica si el nombre ingresado pertenece al diccionario
+		void nombreValido(string *nombre, Diccionario* diccionarioPersonajes);
 
-    void nombreValido(string *nombre, Diccionario* diccionarioPersonajes);
+		// POST: imprime por pantalla si un personaje ya ha sido seleccionado
+		void imprimirUsado();
+	
+		// POST: ejecuta la opcion seleccionada por el usuario del menu Juego.
+		void ejecutarOpcionJuego(Personaje* jugadorUno[MAX_PERSONAJES], int* topeUno, Personaje* jugadorDos[MAX_PERSONAJES],int* topeDos, Diccionario* diccionarioPersonajes);
 
-    void mostrarMenu();
+		// POST: se encarga de procesar el menu del Juego.
+		void procesarMenuJuego(Personaje* jugadorUno[MAX_PERSONAJES],int* topeUno,Personaje* jugadorDos[MAX_PERSONAJES],int* topeDos,Diccionario* diccionarioPersonajes);
 
-    void ejecutarOpcionJuego(Personaje* jugadorUno[MAX_PERSONAJES], int* topeUno, Personaje* jugadorDos[MAX_PERSONAJES],int* topeDos, Diccionario* diccionarioPersonajes);
-
-    void procesarMenuJuego(Personaje* jugadorUno[MAX_PERSONAJES],int* topeUno,Personaje* jugadorDos[MAX_PERSONAJES],int* topeDos,Diccionario* diccionarioPersonajes);
-
-    void imprimirUsado();
-
-
-    ~MenuJuego();
-
-
-
+		// Destructor
+		~MenuJuego();
 };
 
 
