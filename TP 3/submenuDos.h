529130a4e2fd299c3370a8f98f4fe1f7ff326258
ginov777 <78718146+ginov777@users.noreply.github.com>
2021-03-02 13:00:17 -0300

    Update submenuDos.h

--- TP 3/submenuDos.h
+++ TP 3/submenuDos.h
@@ -5,6 +5,8 @@
 
 class SubmenuDos: public Menu {
  public:
+ 
+  //POST: crea un objeto submenudos 
   SubmenuDos();
 
   // POST: muestra el menu por pantalla
@@ -13,6 +15,7 @@ class SubmenuDos: public Menu {
   // POST: ejecuta la opcion elegida.
   void ejecutarOpcion(Personaje* personaje, Personaje* enemigos[MAX_PERSONAJES],Personaje* aliados[MAX_PERSONAJES]);
 
+  //POST: procesa el movimiento del personaje sin chequear el costo
   void moverGratis(Mapa* mapa, Personaje* personaje);
 
   ~SubmenuDos();
