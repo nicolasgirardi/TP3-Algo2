416a27ba6d57d93d01895119dd17ab2066a10b4c
eduescuti <79531987+eduescuti@users.noreply.github.com>
2021-03-02 11:11:31 -0300

    espaciado corregido

--- TP 3/aire.cpp
+++ TP 3/aire.cpp
@@ -1,12 +1,12 @@
 #include "aire.h"
 
 Aire::Aire(string nombre, int escudo, int vida): Personaje(nombre, escudo, vida) {
-  alimento = "aire";
-  defensaActivada = false;
+	alimento = "aire";
+	defensaActivada = false;
 }
 
 void Aire::alimentar() {
-  cout << "No se pueden alimentar los personajes de aire" << endl;
+	cout << "No se pueden alimentar los personajes de aire" << endl;
 }
 
 string Aire::obtenerElemento() { return "aire"; }
@@ -14,12 +14,12 @@ string Aire::obtenerElemento() { return "aire"; }
 string Aire::obtenerAlimento() { return "Este personaje no se puede alimentar"; }
 
 int Aire::obtenerDanio(string elemento) {
-  int danio = 15;
-  if (elemento == "tierra")
-    danio += 5;
-  else if (elemento == "fuego")
-    danio -= 5;
-  return danio;
+	int danio = 15;
+	if (elemento == "tierra")
+		danio += 5;
+	else if (elemento == "fuego")
+		danio -= 5;
+	return danio;
 }
 
 void Aire::recuperarEnergia(){
@@ -27,16 +27,16 @@ void Aire::recuperarEnergia(){
 }
 
 void Aire::ataque(Personaje* enemigos[MAX_PERSONAJES]) {
-  gastarEnergia(ENERGIA_ATAQUE_AIRE);
-  for (int i = 0; i < MAX_PERSONAJES; i++) {
-    enemigos[i]->daniar(this->obtenerDanio(enemigos[i]->obtenerElemento()));
-    cout << this->nombre << " ataco a " << enemigos[i]->obtenerNombre() << " y quedo con " << enemigos[i]->obtenerVida() << " de vida." << endl << endl;
-  }
+	gastarEnergia(ENERGIA_ATAQUE_AIRE);
+	for (int i = 0; i < MAX_PERSONAJES; i++) {
+		enemigos[i]->daniar(this->obtenerDanio(enemigos[i]->obtenerElemento()));
+		cout << this->nombre << " ataco a " << enemigos[i]->obtenerNombre() << " y quedo con " << enemigos[i]->obtenerVida() << " de vida." << endl << endl;
+	}
 }
 
 void Aire::defensa(Personaje* aliados[MAX_PERSONAJES]) {
-  gastarEnergia(ENERGIA_DEFENSA_AIRE);
-  this->defensaActivada = true;
+	gastarEnergia(ENERGIA_DEFENSA_AIRE);
+	this->defensaActivada = true;
 }
 
 void Aire::reseteoDefensa(){
