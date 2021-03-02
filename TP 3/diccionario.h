1cabaeb650a27d1d3a4312354999908ec51fd626
eduescuti <79531987+eduescuti@users.noreply.github.com>
2021-03-02 12:15:33 -0300

    espaciado corregido y metodos documentados

--- TP 3/diccionario.h
+++ TP 3/diccionario.h
@@ -5,70 +5,68 @@
 
 
 class Diccionario {
- private:
-  Nodo* raiz;
-  int cantidad;
+    private:
+        Nodo* raiz;
+        int cantidad;
 
- public:
+    public:
 
-  void todoDetalles(Nodo* d);
+        // POST: crea un diccionario vacío. Deja cantidad en 0 y raíz apunta a NULL.
+        Diccionario();
 
-  // POST: crea un diccionario vacío. Deja cantidad en 0 y raíz apunta a NULL.
-  Diccionario();
+        // POST: devuelve true si el diccionario está vacío
+        bool vacio();
 
-  // POST: devuelve true si el diccionario está vacío
-  bool vacio();
+        // PRE: Dato d es un puntero a un personaje que existe
+        // POST: se crea un nodo con el dato d y se lo ubica en el árbol binario.
+        void alta(Dato d);
 
-  // PRE: Dato d es un puntero a un personaje que existe
-  // POST: se crea un nodo con el dato d y se lo ubica en el árbol binario.
-  void alta(Dato d);
-  
-  // PRE: clave corresponde a un nodo del diccionario
-  //POST: llama a baja con el puntero al personaje cuyo nombre corresponde a la clave ingresada por parámetro
-  void pedirBaja(string clave);
+        // PRE: clave corresponde a un nodo del diccionario
+        //POST: llama a baja con el puntero al personaje cuyo nombre corresponde a la clave ingresada por parámetro
+        void pedirBaja(string clave);
 
-  // PRE: no existe más de un nodo con la misma clave
-  // POST: devuelve el nodo que contiene esta clave
-  Nodo* obtenerNodo(string clave, bool* encontrado);
+        // PRE: no existe más de un nodo con la misma clave
+        // POST: devuelve el nodo que contiene esta clave
+        Nodo* obtenerNodo(string clave, bool* encontrado);
 
-  //PRE: 
-  // POST: devuelve el dato del nodo que contiene la clave ingresada por parámetro
-  Dato consultaNodo(string clave);
+        //PRE: 
+        // POST: devuelve el dato del nodo que contiene la clave ingresada por parámetro
+        Dato consultaNodo(string clave);
 
-  // POST: devuelve la informacion del dato de la clave
-  void consultaInfo(string clave);
+        // POST: devuelve la informacion del dato de la clave
+        void consultaInfo(string clave);
 
-  // POST: devuelve verdadero o falso si hay un nodo con dicha clave
-  bool consultaClave(string clave);
+        void todoDetalles(Nodo* d);
 
-  Nodo* minimo(Nodo* d);
+        // POST: devuelve verdadero o falso si hay un nodo con dicha clave
+        bool consultaClave(string clave);
 
-  void reemplazar(Nodo* nodoViejo, Nodo* nodoNuevo);
+        // POST: devuelve el nodo minimo descendiente del primer nodo que se ingreso
+        Nodo* minimo(Nodo* d);
 
-  // POST: devuelve al nodo raiz del diccionario
-  Nodo* obtenerRaiz();
+        // POST: reemplaza un nodo viejo por uno nuevo
+        void reemplazar(Nodo* nodoViejo, Nodo* nodoNuevo);
 
-  // PRE: 0 < pos <= cantidad
-  // POST: saca el elemento que esta en pos
-  void baja(Nodo* nodoBuscado);
+        // POST: devuelve al nodo raiz del diccionario
+        Nodo* obtenerRaiz();
 
-  // POST: metodo recursivo de imprimirClaves, se encarga de imprimir las
-  // claves de forma recursiva
-  void inOrden(Nodo* d);
+        // PRE: 0 < pos <= cantidad
+        // POST: saca el elemento que esta en pos
+        void baja(Nodo* nodoBuscado);
 
-  // POST: imprime utilizando el metodo inOrden por pantalla todas las claves
-  // del diccionario
-  void imprimirClaves();
+        // POST: metodo recursivo de imprimirClaves, se encarga de imprimir las
+        // claves de forma recursiva
+        void inOrden(Nodo* d);
 
+        // POST: imprime utilizando el metodo inOrden por pantalla todas las claves
+        // del diccionario
+        void imprimirClaves();
 
-  // POST: devuelve la cantidad de nodos
-  int obtenerCantidad();
+        // POST: devuelve la cantidad de nodos
+        int obtenerCantidad();
 
-  void descolgar(Nodo* exhijo,Nodo* padre);
-
-  // POST: libera la memoria
-  virtual ~Diccionario();
-
+        // POST: libera la memoria
+        virtual ~Diccionario();
 };
 
 #endif  // MAIN_CPP_DICCIONARIO_H
