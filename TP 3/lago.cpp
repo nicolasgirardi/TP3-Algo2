8aa3b5efc0cc222e245ef8c101ef3ac198ea98c5
eduescuti <79531987+eduescuti@users.noreply.github.com>
2021-03-02 12:23:59 -0300

    espaciado corregido

--- TP 3/lago.cpp
+++ TP 3/lago.cpp
@@ -1,17 +1,17 @@
 #include "lago.h"
 
 Lago::Lago() : Casilla() {
-  costo = 1;
+    costo = 1;
 }
 
 string Lago::Tipo() { return "lago"; }
 
 int Lago::obtenerCosto(string elemento) {
-  if (elemento == "agua") {
-    return 0;
-  } else if (elemento == "fuego") {
-    return 2;
-  } else {
-    return costo;
-  }
+    if (elemento == "agua") {
+        return 0;
+    } else if (elemento == "fuego") {
+        return 2;
+    } else {
+        return costo;
+    }
 }
