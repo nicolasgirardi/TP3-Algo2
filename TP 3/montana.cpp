da1971e7ed103d2dc7880fbb0498ad9069ec1e90
eduescuti <79531987+eduescuti@users.noreply.github.com>
2021-03-02 12:52:26 -0300

    espaciado corregido

--- TP 3/montana.cpp
+++ TP 3/montana.cpp
@@ -9,9 +9,9 @@ string Montana::Tipo() { return "montana"; }
 int Montana::obtenerCosto(string elemento) {
     if (elemento == "tierra") {
         return 0;
-  	} else if (elemento == "aire") {
-    	return 2;
-  	} else {
-    	return costo;
+    } else if (elemento == "aire") {
+        return 2;
+    } else {
+        return costo;
   	}
 }
