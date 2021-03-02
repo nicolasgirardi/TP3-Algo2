e3c5b92755f79c4cfe4776b3a7828a5c44527f3b
ginov777 <78718146+ginov777@users.noreply.github.com>
2021-03-02 13:05:57 -0300

    Update submenuUno.h

--- TP 3/submenuUno.h
+++ TP 3/submenuUno.h
@@ -5,6 +5,8 @@
 
 class SubmenuUno : public Menu {
  public:
+ 
+  //POST: crea el objeto submenuuno
   SubmenuUno();
 
   // POST: muestra el menu por pantalla
@@ -13,9 +15,10 @@ class SubmenuUno : public Menu {
   // POST: lleva a cabo la alimentación de un personaje a elección
   void alimentarPersonaje(Personaje* personaje);
 
-
+  //POST: mueve un personaje en el mapa si tiene la energía suficiente
   void moverPersonaje(Mapa* mapa, Personaje* personaje, Costos* costos);
 
+  //POST: devuelve los costos de acuerdo al elemento
   Costos* determinarCosto(string elemento, Costos* costos[4]);
 
   // POST: ejecuta la opcion elegida.
