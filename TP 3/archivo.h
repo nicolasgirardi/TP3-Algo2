289c1caa4ca1a6e680b905489e8214f360f7630f
eduescuti <79531987+eduescuti@users.noreply.github.com>
2021-03-02 11:35:07 -0300

    espaciado corregido y metodos documentados

--- TP 3/archivo.h
+++ TP 3/archivo.h
@@ -4,56 +4,72 @@
 #include "personaje.h"
 
 class Archivo {
-private:
-    fstream archivo;
-    string path;
-public:
-    //POST: abre el archivo personajes.csv y avisa en caso de no haberlo podido abrir
-    Archivo();
+    private:
+        fstream archivo;
+        string path;
+    public:
+        //POST: abre el archivo personajes.csv y avisa en caso de no haberlo podido abrir
+        Archivo();
 
-    //POST: devuelve el path del archivo
-    string obtenerPath();
+        //POST: devuelve el path del archivo
+        string obtenerPath();
 
-    void asignarPath(string path);
+        // POST: recibe path y se lo asigna al archivo
+        void asignarPath(string path);
 
-    void abrirArchivoLectura();
+        // POST: abre el archivo en modo lectura
+        void abrirArchivoLectura();
 
-    bool procesarArchivo();
+        // POST: devuelve verdadero en caso de que el archivo exista
+        bool procesarArchivo();
 
-    //POST: devuelve true si es el fin del archivo
-    bool finArchivo();
+        //POST: devuelve true si es el fin del archivo
+        bool finArchivo();
 
-    void abrirArchivoEscritura();
+        // POST: abre el archivo en modo escritura
+        void abrirArchivoEscritura();
 
-    void escribirLinea(Personaje* personaje,int i,int ultimoPersonaje);
+        // POST: escribe una linea en el archivo
+        void escribirLinea(Personaje* personaje,int i,int ultimoPersonaje);
 
-    void escribirArchivo(Personaje* personajesUno[MAX_PERSONAJES], int topeUno, Personaje* personajesDos[MAX_PERSONAJES], int topeDos);
+        // POST: escribe el archivo
+        void escribirArchivo(Personaje* personajesUno[MAX_PERSONAJES], int topeUno, Personaje* personajesDos[MAX_PERSONAJES], int topeDos);
 
-    void leerElemento(string* elemento, char delim);
+        // POST: lee el elemento
+        void leerElemento(string* elemento, char delim);
 
-    void leerNombre(string* nombre, char delim);
+        // POST: lee el nombre
+        void leerNombre(string* nombre, char delim);
 
-    void leerEscudo(int* escudo, char delim);
+        // POST: lee el escudo
+        void leerEscudo(int* escudo, char delim);
 
-    void leerVida(int* vida, char delim);
+        // POST: lee la vida
+        void leerVida(int* vida, char delim);
 
-    void leerEnergia(int* energia, char delim);
+        // POST: lee la energia
+        void leerEnergia(int* energia, char delim);
 
-    void leerFila(int* fila, char delim);
+        // POST: lee la fila
+        void leerFila(int* fila, char delim);
 
-    void leerColumna(int* columna);
+        // POST: lee la columna
+        void leerColumna(int* columna);
 
-    //POST: guarda los strings separados por ; y convierte en entero el string escudo y vida
-    void descomponerLineaBasica(string* elemento, string* nombre, int* escudo, int* vida);
+        //POST: guarda los strings separados por ; y convierte en entero el string escudo y vida
+        void descomponerLineaBasica(string* elemento, string* nombre, int* escudo, int* vida);
 
-    void descomponerLineaPartida(string* elemento, string* nombre, int* escudo, int* vida, int* energia, int* fila, int* columna);
+        // POST: guarda los strings separados por ; y convierte en entero el string escudo, vida, energia, fila y columna
+        void descomponerLineaPartida(string* elemento, string* nombre, int* escudo, int* vida, int* energia, int* fila, int* columna);
 
-    void cerrarArchivo();
+        // POST: cierra el archivo
+        void cerrarArchivo();
 
-    void eliminarArchivo();
+        // POST: se ocupa de eliminar el archivo
+        void eliminarArchivo();
 
-    //POST: cierra el archivo
-    ~Archivo();
+        //POST: cierra el archivo
+        ~Archivo();
 };
 
 
