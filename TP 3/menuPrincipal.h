13f6a7a385ecac154b1eb2f81ea8d391acd8d679
ginov777 <78718146+ginov777@users.noreply.github.com>
2021-03-02 12:57:51 -0300

    Update menuPrincipal.h

--- TP 3/menuPrincipal.h
+++ TP 3/menuPrincipal.h
@@ -7,37 +7,46 @@
 
 class MenuPrincipal: public Menu {
 private:
-
-public:  //VER CUALES PONER PRIVATE
-
-    MenuPrincipal();
-
-
-    void procesarMenuPrincipal(char* estadoJuego,Diccionario* diccionarioPersonajes);
-
+	//POST: deja inicializada la vida con un valor válido
+    void cargarVida(int* vida);
+	
+	//POST:deja inicializado el escudo con un valor válido
+    void determinarEscudo(int* escudo);
+    
+    //POST: devuelve true si es uno de los 4 elementos del juego: aire, tierra, agua, fuego
+    bool elementoValido(string elemento);
+    
+    //POST: pide por pantalla al usuario ingresar un elemento válido
+    void pedirElemento(string* elemento);
+	
+	//POST: procesa el agregado del personaje
     void agregarPersonaje(Diccionario* diccionarioPersonajes);
-
+	
+	//POST: da de baja un personaje del diccionario
     void eliminarPersonaje(Diccionario* diccionarioPersonajes);
 
+	//POST: cambia el estado de juego a jugando
     void iniciarJuego(char* estadoJuego);
-
+	
+	//POST: llamada a la función correspondiente de acuerdo a la opcion elegida
     void ejecutarOpcionPrincipal(char* estadoJuego,Diccionario* diccionarioPersonajes);
 
+	//POST: crea un personaje en la memoria dinámica y los da de alta en el diccionario
     void crearPersonaje(string elemento, string nombre, int escudo, int vida, Diccionario* diccionarioPersonajes);
-
-
+	
+	//POST: imprime por pantalla el menu
     void mostrarMenu();
+	
+public:  
 
-    void determinarEscudo(int* escudo);
+	//POST: crea un objeto menu con la opcion en 0
+    MenuPrincipal();
 
-    void cargarVida(int* vida);
+	//POST: procesa el menu hasta que opcion sea SALIR o Empezar juego
+    void procesarMenuPrincipal(char* estadoJuego,Diccionario* diccionarioPersonajes);
 
-    bool elementoValido(string elemento);
-
-    void pedirElemento(string* elemento);
-
+    //POST: procesa el pedido del nombre hasta que sea válido (no se encuentre en el diccionario)
     void ingresarNombre(string* nombre,Diccionario* diccionarioPersonajes);
-
 
     ~MenuPrincipal();
 
