bd9a46c127d99d89ade47008040e883bb635d7f2
eduescuti <79531987+eduescuti@users.noreply.github.com>
2021-03-02 11:41:31 -0300

    espaciado corregido y metodos documentados

--- TP 3/casilla.h
+++ TP 3/casilla.h
@@ -5,43 +5,39 @@
 #include "personaje.h"
 
 class Casilla {
- protected:
-  Personaje* ocupante;
+    protected:
+        
+        Personaje* ocupante;
 
- public:
-  // PRE:
-  // POST: construye una casilla sin ocupante
-  Casilla();
+    public:
+        // PRE:
+        // POST: construye una casilla sin ocupante
+        Casilla();
 
-  // verifica si la casilla esta ocupada o no
-  // PRE:
-  // pos:devuelve true si esta ocupada, false si no
+        // Verifica si la casilla esta ocupada o no
+        // PRE:
+        // POST:devuelve true si esta ocupada, false si no
+        bool ocupacion();
 
-  bool ocupacion();
+        // Ocupa la casilla con un personaje
+        // PRE: Ocupaci�n debe ser falso
+        // POST: pone a un personaje que se pasa dentro de la casilla
+        void ocupar(Personaje* personaje);
 
-  // ocupa la casilla con un personaje
-  // PRE: Ocupaci�n debe ser falso
-  // POS: pone a un personaje que se pasa dentro de la casilla
+        // Consulta que personaje esta en la casilla
+        // PRE: ocupacion debe ser true
+        // POST: devuelve el puntero al personaje de la casilla
+        Personaje* obtenerPersonaje();
 
-  void ocupar(Personaje* personaje);
+        // Vacia la casilla de quien la ocupe
+        // PRE:
+        // POST: ocupante pasa a apuntar a null
+        void vaciar();
 
-  // consulta que personaje esta en la casilla
-  // PRE: ocupacion debe ser true
-  // POS: devuelve el puntero al personaje de la casilla
-
-  Personaje* obtenerPersonaje();
-
-  // vacia la casilla de quien la ocupe
-  // pre:
-  // pos: ocupante pasa a apuntar a null
-
-  void vaciar();
-
-  // devuelve el coste de llegar a esta casilla desde una vecina para cada
-  // elemento pre: pos: devuelve el coste de llegar a esta casilla desde una
-  // vecina
-
-  virtual int obtenerCosto(string elemento) = 0;
+        // Devuelve el coste de llegar a esta casilla desde una vecina para cada elemento
+        // PRE:
+        // POST: devuelve el coste de llegar a esta casilla desde una vecina
+        virtual int obtenerCosto(string elemento) = 0;
 };
 
 #endif  // CASILLA_H_INCLUDED
